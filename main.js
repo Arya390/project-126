3210bed24dbe8aadeab7be52ceac0c62b421988b
Arya390 <75195330+Arya390@users.noreply.github.com>
2021-09-08 15:44:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -28,22 +28,22 @@ function modelLoaded() {
     console.log('PoseNet Is Initialized');
 }
 
-function gotPoses(results) {
-    if (results.length > 0) {
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
         console.log(results);
-
-        leftWristscore = results[0].pose.keypoints[9].score;
         rightWristscore = results[0].pose.keypoints[10].score;
+        leftWristscore = results[0].pose.keypoints[9].score;
         console.log("rightWristscore = " + rightWristscore + "leftWristscore = " + leftWristscore);
-
-        leftWristX = results[0].pose.leftWrist.x;
-        leftWristY = results[0].pose.leftWrist.y;
-        console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
 
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
 
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
     }
 }
 
